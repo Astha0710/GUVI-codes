f5c98a1051f6a8447a9a664067b35f715392efb2
Astha0710 <43231180+Astha0710@users.noreply.github.com>
2019-03-06 01:54:39 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+class Ideone
+{
+
+	
+public static void main (String[] args) throws java.lang.Exception
+
+{
+	
+	
+Scanner sc=new Scanner(System.in);
+	
+int N=sc.nextInt();
+	
+for(int i=0;i<N;i++)
+	
+{
+	    
+System.out.println("Hello");
+	
+}
+    
+
+}
+
+}
