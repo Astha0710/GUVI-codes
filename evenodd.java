74a6e8f4de6a665e20c82ecac2b986324b0b0461
Astha0710 <43231180+Astha0710@users.noreply.github.com>
2019-03-06 01:42:29 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+{
+
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+	
+Scanner sc=new Scanner(System.in);
+	
+int T=sc.nextInt();
+	
+if(T%2==0 && T>=0)
+	
+{
+		
+System.out.println("Even");
+	
+}
+	
+else if(T%2!=0 && T>=0)
+	
+{
+		
+System.out.println("Odd");
+	
+}
+	
+else
+	
+{
+		
+System.out.println("invalid");
+	
+}
+	
+}
+}
