8a81c006965a8a8bc25d82045d286d5b62791c80
Astha0710 <43231180+Astha0710@users.noreply.github.com>
2019-03-06 02:02:54 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+import java.util.*;
+
+import java.lang.*;
+
+
+
+class Ideone
+{
+
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+    
+	
+Scanner sc=new Scanner(System.in);
+	
+int N=sc.nextInt();
+	
+int P=sc.nextInt();
+   
+ int res=1;
+    
+for(int i=0;i<P;i++)
+   
+ {
+        
+res=res*N;
+   
+ }
+    
+System.out.println(res);
+	
+
+}
+
+}
