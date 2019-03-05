d51f272c564ea56125e8ac44a534d3e778ae6036
Astha0710 <43231180+Astha0710@users.noreply.github.com>
2019-03-06 01:44:41 +0530

    Create alphabe.java

---
+++ alphabe.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+{
+
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+	
+Scanner sc=new Scanner(System.in);
+	
+char T=sc.next().charAt(0);
+	
+if(T>='a' && T<='z'|| T>='A' && T<='Z')
+	
+{
+		
+System.out.println("Alphabet");
+	
+}
+	
+else
+	
+{
+		
+System.out.println("no");
+	
+}
+
+	
+}
+
+}
