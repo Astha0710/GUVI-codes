76068ef98c09ebba65c6e8501497c833bd77f327
Astha0710 <43231180+Astha0710@users.noreply.github.com>
2019-03-06 02:17:09 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+import java.util.*;
+
+import java.lang.*;
+
+
+
+class Ideone
+{
+
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+    
+	
+Scanner sc=new Scanner(System.in);
+	
+int N=sc.nextInt();
+	
+int K=sc.nextInt();
+	
+
+	
+int i=0;
+	
+for(i=N+1;i<K;i++)
+	
+{
+	    
+if(i%2==0)
+	   
+ {
+	        
+System.out.print(i+" ");
+	    
+}
+	
+}
+
+}
+}
