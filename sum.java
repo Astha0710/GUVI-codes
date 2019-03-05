b0ff1257615ae393686746438d9927d81519f698
Astha0710 <43231180+Astha0710@users.noreply.github.com>
2019-03-06 01:56:03 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+
+class Ideone
+{
+
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+	
+int sum=0;
+	
+Scanner sc=new Scanner(System.in);
+	
+int N=sc.nextInt();
+	
+for(int i=1;i<=N;i++)
+	
+{
+	   
+sum=sum+i; 
+	
+}
+ 
+System.out.println(sum);
+}
+
+}
