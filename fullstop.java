fab9af1a124e4aa92debebf0287ed36509cf9caf
Astha0710 <43231180+Astha0710@users.noreply.github.com>
2019-03-07 22:35:48 +0530

    Create fullstop.java

---
+++ fullstop.java
@@ -0,0 +1,29 @@
+
+import java.io.*;
+
+import java.lang.*;
+
+import java.util.*;
+
+
+class FullStop {
+	
+public static void main (String[] args) 
+{
+	    
+int j=0;
+	    
+Scanner sc=new Scanner(System.in);
+	   
+String s=sc.next();
+	   
+ String b=".";
+	    
+b=s+b;
+	    
+	    
+		
+System.out.println(b);
+	
+}
+}
