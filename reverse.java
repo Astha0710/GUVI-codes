958beda416e4e17a3fc6e631bcd94ceec156e983
Astha0710 <43231180+Astha0710@users.noreply.github.com>
2019-03-07 22:31:10 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+import java.lang.*;
+
+import java.util.*;
+
+class Reverse{
+   
+ public static void main(String args[])
+    
+   
+ {String rev=" ";
+        
+Scanner sc=new Scanner(System.in);
+        
+String s=sc.nextLine();
+        
+char[] c=s.toCharArray();
+       
+ for(int i=c.length-1;i>=0;i--)
+        
+{
+            
+System.out.print(c[i]);
+        
+}
+    
+}
+
+}
