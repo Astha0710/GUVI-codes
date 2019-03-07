5e91e92b4d7d75ed1e4248a232fff8772b35852a
Astha0710 <43231180+Astha0710@users.noreply.github.com>
2019-03-07 22:32:43 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+import java.lang.*;
+
+import java.util.*;
+
+class Fact{
+    
+public static void main(String args[])
+    
+    
+{
+      
+Scanner sc=new Scanner(System.in);
+       
+ int s=sc.nextInt();
+       
+int f=1;
+        
+for(int i=1;i<=s;i++)
+        
+{
+            
+f=f*i;
+            
+       
+ }
+        
+System.out.print(f);
+    
+}
+
+}
