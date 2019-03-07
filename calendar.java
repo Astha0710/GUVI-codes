1beff62e55b122c458a5c63d56f0b252635318ff
Astha0710 <43231180+Astha0710@users.noreply.github.com>
2019-03-07 22:39:19 +0530

    Create calendar.java

---
+++ calendar.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Holidays 
+{
+    public static void main(String args[])
+{
+    
+       Scanner sc=new Scanner(System.in);
+      String l=sc.next();
+      if(l.equals("Sunday"))
+      {
+          System.out.println("yes");
+      }
+        else if(l.equals("Saturday"))
+      {
+          System.out.println("yes");
+      }
+       
+     else
+      {
+          System.out.println("no");
+      }
+      
+        
+    
+        
+    } 
+}
